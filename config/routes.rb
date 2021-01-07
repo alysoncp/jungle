6216ca47e09ad9c436a95924fbaa6260324f97f7
alysoncp <alyson.c.pickett@gmail.com>
2021-01-07 17:30:18 +0000

    user authentication added with simple auth tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,17 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
+  #User signup routes
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+   # these routes are for showing users a login form, logging them in, and logging them out.
+   get '/login' => 'sessions#new'
+   post '/login' => 'sessions#create'
+   get '/logout' => 'sessions#destroy'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
